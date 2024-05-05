48f1d816574e16c0c1cfe48e1c2caf75404740ad
Sardar Badar Saghir <60216738+BadarSaghir@users.noreply.github.com>
2024-05-05 18:19:46 +0500

    added github

---
+++ wizterm/wezterm/config/appearance.lua
@@ -0,0 +1,56 @@
+local wezterm = require('wezterm')
+local gpu_adapters = require('utils.gpu_adapter')
+local colors = require('colors.custom')
+
+return {
+   animation_fps = 60,
+   max_fps = 60,
+   front_end = 'WebGpu',
+   webgpu_power_preference = 'HighPerformance',
+   webgpu_preferred_adapter = gpu_adapters:pick_best(),
+
+   -- color scheme
+   colors = colors,
+
+   -- background
+   background = {
+      {
+         source = { File = wezterm.GLOBAL.background },
+      },
+      {
+         source = { Color = colors.background },
+         height = '100%',
+         width = '100%',
+         opacity = 0.96,
+      },
+   },
+
+   -- scrollbar
+   enable_scroll_bar = true,
+
+   -- tab bar
+   enable_tab_bar = true,
+   hide_tab_bar_if_only_one_tab = false,
+   use_fancy_tab_bar = false,
+   tab_max_width = 25,
+   show_tab_index_in_tab_bar = false,
+   switch_to_last_active_tab_when_closing_tab = true,
+
+   -- window
+   window_padding = {
+      left = 5,
+      right = 10,
+      top = 12,
+      bottom = 7,
+   },
+   window_close_confirmation = 'NeverPrompt',
+   window_frame = {
+      active_titlebar_bg = '#090909',
+      -- font = fonts.font,
+      -- font_size = fonts.font_size,
+   },
+   inactive_pane_hsb = {
+      saturation = 0.9,
+      brightness = 0.65,
+   },
+}
---
+++ wizterm/wezterm/config/bindings.lua
@@ -0,0 +1,215 @@
+local wezterm = require('wezterm')
+local platform = require('utils.platform')()
+local backdrops = require('utils.backdrops')
+local act = wezterm.action
+
+local mod = {}
+
+if platform.is_mac then
+   mod.SUPER = 'SUPER'
+   mod.SUPER_REV = 'SUPER|CTRL'
+elseif platform.is_win or platform.is_linux then
+   mod.SUPER = 'ALT' -- to not conflict with Windows key shortcuts
+   mod.SUPER_REV = 'ALT|CTRL'
+end
+
+-- stylua: ignore
+local keys = {
+   -- misc/useful --
+   { key = 'F1', mods = 'NONE', action = 'ActivateCopyMode' },
+   { key = 'F2', mods = 'NONE', action = act.ActivateCommandPalette },
+
+   { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
+
+    {
+        key = "F12",
+    mods="NONE",
+        action = wezterm.action_callback(function(_, pane)
+            local tab = pane:tab()
+            local panes = tab:panes_with_info()
+            if #panes == 1 then
+                pane:split({
+                    direction = "Right",
+                    size = 0.4,
+                })
+            elseif not panes[1].is_zoomed then
+                panes[1].pane:activate()
+                tab:set_zoomed(true)
+            elseif panes[1].is_zoomed then
+                tab:set_zoomed(false)
+                panes[2].pane:activate()
+            end
+        end),
+    },
+  
+  {key = 'F11', mods="NONE",action = act.TogglePaneZoomState},
+
+
+   { key = 'F4', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
+   {
+      key = 'F5',
+      mods = 'NONE',
+      action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }),
+   },
+   -- toggle fullscreen
+ 
+   { key = 'f',   mods = mod.SUPER, action = act.Search({ CaseInSensitiveString = '' }) },
+   {
+      key = 'u',
+      mods = mod.SUPER,
+      action = wezterm.action.QuickSelectArgs({
+         label = 'open url',
+         patterns = {
+            'https?://\\S+',
+         },
+         action = wezterm.action_callback(function(window, pane)
+            local url = window:get_selection_text_for_pane(pane)
+            wezterm.log_info('opening: ' .. url)
+            wezterm.open_with(url)
+         end),
+      }),
+   },
+
+   -- copy/paste --
+   { key = 'c', mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
+   { key = 'v', mods = 'CTRL|SHIFT',  action = act.PasteFrom('Clipboard') },
+
+   -- tabs --
+   -- tabs: spawn+close
+   { key = 't', mods = mod.SUPER,     action = act.SpawnTab('DefaultDomain') },
+   { key = 't', mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = 'WSL:Ubuntu' }) },
+   { key = 'w', mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
+
+   -- tabs: navigation
+   { key = '[', mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
+   { key = ']', mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
+   { key = '[', mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
+   { key = ']', mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+
+   -- window --
+   -- spawn windows
+   { key = 'n', mods = mod.SUPER,     action = act.SpawnWindow },
+
+   -- background controls --
+   {
+      key = [[/]],
+      mods = mod.SUPER,
+      action = wezterm.action_callback(function(window, _pane)
+         backdrops:random(window)
+      end),
+   },
+   {
+      key = [[,]],
+      mods = mod.SUPER,
+      action = wezterm.action_callback(function(window, _pane)
+         backdrops:cycle_back(window)
+      end),
+   },
+   {
+      key = [[.]],
+      mods = mod.SUPER,
+      action = wezterm.action_callback(function(window, _pane)
+         backdrops:cycle_forward(window)
+      end),
+   },
+   {
+      key = [[/]],
+      mods = mod.SUPER_REV,
+      action = act.InputSelector({
+         title = 'Select Background',
+         choices = backdrops:choices(),
+         fuzzy = true,
+         fuzzy_description = 'Select Background: ',
+         action = wezterm.action_callback(function(window, _pane, idx)
+            ---@diagnostic disable-next-line: param-type-mismatch
+            backdrops:set_img(window, tonumber(idx))
+         end),
+      }),
+   },
+
+   -- panes --
+   -- panes: split panes
+   {
+      key = [[\]],
+      mods = mod.SUPER,
+      action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+   },
+   {
+      key = [[\]],
+      mods = mod.SUPER_REV,
+      action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+   },
+
+   -- panes: zoom+close pane
+   { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
+   { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
+
+   -- panes: navigation
+   { key = 'k',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
+   { key = 'j',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Down') },
+   { key = 'h',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Left') },
+   { key = 'l',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Right') },
+   {
+      key = 'p',
+      mods = mod.SUPER_REV,
+      action = act.PaneSelect({ alphabet = '1234567890', mode = 'SwapWithActiveKeepFocus' }),
+   },
+
+   -- key-tables --
+   -- resizes fonts
+   {
+      key = 'f',
+      mods = 'LEADER',
+      action = act.ActivateKeyTable({
+         name = 'resize_font',
+         one_shot = false,
+         timemout_miliseconds = 1000,
+      }),
+   },
+   -- resize panes
+   {
+      key = 'p',
+      mods = 'LEADER',
+      action = act.ActivateKeyTable({
+         name = 'resize_pane',
+         one_shot = false,
+         timemout_miliseconds = 1000,
+      }),
+   },
+}
+
+-- stylua: ignore
+local key_tables = {
+   resize_font = {
+      { key = 'k',      action = act.IncreaseFontSize },
+      { key = 'j',      action = act.DecreaseFontSize },
+      { key = 'r',      action = act.ResetFontSize },
+      { key = 'Escape', action = 'PopKeyTable' },
+      { key = 'q',      action = 'PopKeyTable' },
+   },
+   resize_pane = {
+      { key = 'k',      action = act.AdjustPaneSize({ 'Up', 1 }) },
+      { key = 'j',      action = act.AdjustPaneSize({ 'Down', 1 }) },
+      { key = 'h',      action = act.AdjustPaneSize({ 'Left', 1 }) },
+      { key = 'l',      action = act.AdjustPaneSize({ 'Right', 1 }) },
+      { key = 'Escape', action = 'PopKeyTable' },
+      { key = 'q',      action = 'PopKeyTable' },
+   },
+}
+
+local mouse_bindings = {
+   -- Ctrl-click will open the link under the mouse cursor
+   {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = act.OpenLinkAtMouseCursor,
+   },
+}
+
+return {
+   disable_default_key_bindings = true,
+   leader = { key = 'Space', mods = mod.SUPER_REV },
+   keys = keys,
+   key_tables = key_tables,
+   mouse_bindings = mouse_bindings,
+}
