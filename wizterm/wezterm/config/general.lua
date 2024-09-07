f49658b928361be1963e87e57087b8e3edb799e6
Sardar Badar Saghir <60216738+BadarSaghir@users.noreply.github.com>
2024-09-07 16:20:01 +0500

    Update general.lua

--- wizterm/wezterm/config/general.lua
+++ wizterm/wezterm/config/general.lua
@@ -1,7 +1,7 @@
 return {
    -- behaviours
    automatically_reload_config = true,
-   exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
+   exit_behavior = 'Close', -- if the shell program exited with a successful status
    status_update_interval = 1000,
 
    scrollback_lines = 5000,
