5dad2ac237d2715c52f732e6fc5a291f79674ed0
Sardar Badar Saghir <60216738+BadarSaghir@users.noreply.github.com>
2024-09-07 16:19:26 +0500

    Update appearance.lua

--- wizterm/wezterm/config/appearance.lua
+++ wizterm/wezterm/config/appearance.lua
@@ -1,56 +1,56 @@
-local wezterm = require('wezterm')
-local gpu_adapters = require('utils.gpu_adapter')
-local colors = require('colors.custom')
+local wezterm = require("wezterm")
+local gpu_adapters = require("utils.gpu_adapter")
+local colors = require("colors.custom")
 
 return {
-   animation_fps = 60,
-   max_fps = 60,
-   front_end = 'WebGpu',
-   webgpu_power_preference = 'HighPerformance',
-   webgpu_preferred_adapter = gpu_adapters:pick_best(),
+	animation_fps = 60,
+	max_fps = 60,
+	front_end = "WebGpu",
+	webgpu_power_preference = "HighPerformance",
+	webgpu_preferred_adapter = gpu_adapters:pick_best(),
 
-   -- color scheme
-   colors = colors,
+	-- color scheme
+	colors = colors,
 
-   -- background
-   background = {
-      {
-         source = { File = wezterm.GLOBAL.background },
-      },
-      {
-         source = { Color = colors.background },
-         height = '100%',
-         width = '100%',
-         opacity = 0.96,
-      },
-   },
+	-- background
+	background = {
+		{
+			source = { File = wezterm.GLOBAL.background },
+		},
+		{
+			source = { Color = colors.background },
+			height = "100%",
+			width = "100%",
+			opacity = 0.96,
+		},
+	},
 
-   -- scrollbar
-   enable_scroll_bar = false,
+	-- scrollbar
+	enable_scroll_bar = false,
 
-   -- tab bar
-   enable_tab_bar = false,
-   hide_tab_bar_if_only_one_tab = true,
-   use_fancy_tab_bar = false,
-   tab_max_width = 25,
-   show_tab_index_in_tab_bar = false,
-   switch_to_last_active_tab_when_closing_tab = true,
-
-   -- window
-   window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0,
-   },
-   window_close_confirmation = 'NeverPrompt',
-   window_frame = {
-      active_titlebar_bg = '#090909',
-      -- font = fonts.font,
-      -- font_size = fonts.font_size,
-   },
-   inactive_pane_hsb = {
-      saturation = 0.9,
-      brightness = 0.65,
-   },
+	-- tab bar
+	enable_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = false,
+	tab_max_width = 25,
+	show_tab_index_in_tab_bar = false,
+	switch_to_last_active_tab_when_closing_tab = true,
+	--	window_decorations = "NONE",
+	-- window
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	window_close_confirmation = "NeverPrompt",
+	window_frame = {
+		active_titlebar_bg = "#090909",
+		-- font = fonts.font,
+		-- font_size = fonts.font_size,
+	},
+	inactive_pane_hsb = {
+		saturation = 0.9,
+		brightness = 0.65,
+	},
 }
